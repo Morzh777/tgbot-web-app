55335d9451addcc3f717617b8ca199a464c7cc32
Morzh777 <m89991268457@gmail.com>
2025-06-16 19:36:21 +0300

    Trigger Vercel redeploy to fix build error

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Настройки для GitHub Pages
-  output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/tgbot-web-app' : '',
-  images: {
-    unoptimized: true
-  }
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig; 
